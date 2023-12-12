ddbc181eb06adfe72e53f1a9c031e3dcd3b89495
hruizrmz <hruizrmz@gmail.com>
2023-12-12 23:57:17 +0200

    rendering practice
    - Added a buster sword model from last semester
    - Z axis rotation and floating
    - Color brightness distortion

--- demos/simple-rendering.js
+++ demos/simple-rendering.js
@@ -11,19 +11,7 @@ import {mat4, vec3, vec4} from "../node_modules/gl-matrix/esm/index.js";
 // **                       Data                       **
 // ******************************************************
 
-//         -.5 .5 -.5  +--------------+ .5 .5 -.5
-//                    /|             /|
-//                   / |            / |
-//      -.5 .5 .5   *--+-----------*  | .5 .5 .5
-//                  |  |           |  |
-//                  |  |           |  |
-//                  |  |           |  |
-//     -.5 -.5 -.5  |  +-----------+--+ .5 -.5 -.5
-//                  | /            | /
-//                  |/             |/
-//     -.5 -.5 .5   *--------------*  .5 -.5 .5
-
-import {positions, normals, indices} from "../blender/cube.js"
+import {positions, normals, indices} from "../blender/bustersword.js"
 
 // ******************************************************
 // **               Geometry processing                **
@@ -46,7 +34,9 @@ let vertexShader = `
     
     void main()
     {
-        gl_Position = modelViewProjectionMatrix * vec4(position, 1.0);
+        float zFloat = sin(time) * 0.2;
+        vec3 newPos = position + vec3(0, 0, zFloat);
+        gl_Position = modelViewProjectionMatrix * vec4(newPos, 1.0);
         vec3 viewNormal = (modelViewMatrix * vec4(normal, 0.0)).xyz;
         color = mix(bgColor * 0.8, fgColor, viewNormal.z) + pow(viewNormal.z, 20.0);
     }
@@ -68,7 +58,7 @@ let fragmentShader = `
     
     void main()
     {
-        outColor = color;
+        outColor = color + (cos(time));
     }
 `;
 
@@ -76,8 +66,8 @@ let fragmentShader = `
 // **             Application processing               **
 // ******************************************************
 
-let bgColor = vec4.fromValues(1.0, 0.2, 0.3, 1.0);
-let fgColor = vec4.fromValues(1.0, 0.9, 0.5, 1.0);
+let bgColor = vec4.fromValues(0.51, 0.68, 0.53, 1.0); // pastel green
+let fgColor = vec4.fromValues(0.31, 0.31, 0.31, 1.0); // dark gray
 
 
 app.clearColor(bgColor[0], bgColor[1], bgColor[2], bgColor[3])
@@ -99,6 +89,7 @@ let modelViewMatrix = mat4.create();
 let modelViewProjectionMatrix = mat4.create();
 let rotateXMatrix = mat4.create();
 let rotateYMatrix = mat4.create();
+let rotateZMatrix = mat4.create();
 
 let drawCall = app.createDrawCall(program, vertexArray)
     .uniform("bgColor", bgColor)
@@ -107,13 +98,16 @@ let drawCall = app.createDrawCall(program, vertexArray)
 function draw(timems) {
     const time = timems * 0.001;
 
-    mat4.perspective(projMatrix, Math.PI / 4, app.width / app.height, 0.1, 100.0);
-    mat4.lookAt(viewMatrix, vec3.fromValues(3, 0, 2), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
+    mat4.perspective(projMatrix, Math.PI / 7, app.width / app.height, 0.1, 100.0);
+    // eye: slightly away from the center
+    mat4.lookAt(viewMatrix, vec3.fromValues(0, 5, -0.1), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
     mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
 
-    mat4.fromXRotation(rotateXMatrix, time * 0.1136);
-    mat4.fromYRotation(rotateYMatrix, time * 0.2235);
+    mat4.fromXRotation(rotateXMatrix, 0);
+    mat4.fromYRotation(rotateYMatrix, 5 * Math.PI / 4); // 150 degrees
+    mat4.fromZRotation(rotateZMatrix, time * 2);
     mat4.multiply(modelMatrix, rotateXMatrix, rotateYMatrix);
+    mat4.multiply(modelMatrix, modelMatrix, rotateZMatrix);
 
     mat4.multiply(modelViewMatrix, viewMatrix, modelMatrix);
     mat4.multiply(modelViewProjectionMatrix, viewProjMatrix, modelMatrix);
